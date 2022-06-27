abf359f3d706db63d82ff7f28f67d54b72666de2
Pooja Umekar <poojaumekar123@gmail.com>
2022-06-27 09:47:37 +0530

    Initial commit

---
+++ src/test/java/com/practice/MavenProjectInEsclipse_1/CapturingScreenShotTest.java
@@ -0,0 +1,12 @@
+package com.practice.MavenProjectInEsclipse_1;
+
+import org.testng.annotations.Test;
+
+public class CapturingScreenShotTest {
+@Test
+public void CaptureScreenShot()
+{
+	System.out.println("Capturing ScreenShot In Case of an error");
+}
+}
+
---
+++ src/test/java/com/practice/MavenProjectInEsclipse_1/SendingMailTest.java
@@ -0,0 +1,5 @@
+package com.practice.MavenProjectInEsclipse_1;
+
+public class SendingMailTest {
+
+}
